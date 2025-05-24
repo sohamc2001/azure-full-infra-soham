4d721fd261a521c79a2619d3716984b5296bbb00
Soham Chakraborty <104936089+sohamc2001@users.noreply.github.com>
2025-05-25 04:06:38 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,11 +3,6 @@ output "vm_public_ip" {
   value       = azurerm_public_ip.public_ip.ip_address
 }
 
-output "app_service_default_hostname" {
-  description = "App Service URL"
-  value       = azurerm_app_service.app.default_site_hostname
-}
-
 output "vm_admin_password" {
   description = "Admin password for VM"
   value       = random_password.password.result
